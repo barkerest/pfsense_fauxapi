a2a119066a2286198f48e8ac50bb40f600b06745
Beau Barker <bbarker@ckindustrial.com>
2020-03-02 11:18:37 -0500

    Update build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-#/bin/sh
+#/bin/csh
 
 set PKG_ROOT=`pwd`
 cd pfSense-pkg-FauxAPI
-make clean && make package && mv work/pkg/pfSense-pkg-FauxAPI*.txz "${PKG_ROOT}/packages"
+make clean && make package
+if ($? == 0) then
+  cd work/pkg
+  set PKG_NAME=`ls -b *.txz`
+  mv $PKG_NAME "${PKG_ROOT}/packages/${PKG_NAME}"
+  echo $PKG_NAME > "${PKG_ROOT}/packages/LATEST"
+endif
 cd $PKG_ROOT
